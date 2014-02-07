c44fcaf6488f4ddeff5ce753755ac289a3742a5d
Adam Collard <adam.collard@canonical.com>
2014-02-07 00:26:04 +0000

    Corrected cinder in bundles when not backed by Ceph.

--- openstack/havana-compute/03configure-cloud.sh
+++ openstack/havana-compute/03configure-cloud.sh
@@ -1,1 +1,14 @@
-../havana/03configure-cloud.sh
+#!/bin/bash
+
+echo "Configuring Openstack environment"
+cd ../openstack-configuration/
+
+[ -f ~/images/precise-server-cloudimg-amd64-disk1.img ] || ./get-cloud-images
+
+./inspect-environment
+source envrc
+
+./prepare-cloud havana
+
+echo "Skipping Juju config as no Object Storage available for Juju"
+
