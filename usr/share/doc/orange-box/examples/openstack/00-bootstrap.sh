c684b4495046afc58067327fa67afd4fa4ce1000
Dustin Kirkland <dustin.kirkland@gmail.com>
2014-05-05 22:57:19 -0500

    * usr/share/doc/orange-box/examples/openstack/00-bootstrap.sh:   - clean up bootstrap a bit * debian/postinst, usr/bin/orange-box-add-virtual-nodes,   usr/share/doc/orange-box/examples/openstack/00-bootstrap.sh:   - set MAAS upstream DNS   - fix typos in virtual node add   - simplify openstack bootstrap

--- usr/share/doc/orange-box/examples/openstack/00-bootstrap.sh
+++ usr/share/doc/orange-box/examples/openstack/00-bootstrap.sh
@@ -4,19 +4,9 @@
 
 set -x
 
-echo "Demo prep runtime is about 5 minutes"
-echo "checking for existing Juju environment..."
-juju status && echo "Existing deployment found, exiting." && exit
-
-echo "No existing environment found, we are good to go."
-# Blow away the old known_hosts and juju environments to prevent
-# SSH key errors and any leftovers from previous deployments
-rm -rf ~/.ssh/known_hosts
-rm -rf ~/.juju/environments
+juju status && echo "Existing deployment found, exiting." && exit 0
 juju bootstrap --constraints "tags=virtual" --show-log --upload-tools
 juju set-constraints "tags="
-#echo "Waiting for bootstrap node to deploy (5 minutes)"
-#sleep 300
 juju deploy --to 0 --repository=/srv/charmstore/ local:precise/juju-gui
 juju expose juju-gui
 
