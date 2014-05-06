6352da0bc720737efe76fe6aa744eb888b5b9cda
Dustin Kirkland <dustin.kirkland@gmail.com>
2014-05-06 00:00:12 -0500

    * debian/postinst, debian/preinst, usr/share/doc/orange-   box/examples/openstack/00-bootstrap.sh:   - revert the virtual machine installation and the bridge creation     + bridge is causing serious problems with network manager

--- usr/share/doc/orange-box/examples/openstack/00-bootstrap.sh
+++ usr/share/doc/orange-box/examples/openstack/00-bootstrap.sh
@@ -5,8 +5,9 @@
 set -x
 
 juju status && echo "Existing deployment found, exiting." && exit 0
-juju bootstrap --constraints "tags=virtual" --show-log --upload-tools
-juju set-constraints "tags="
+#juju bootstrap --constraints "tags=virtual" --show-log --upload-tools
+#juju set-constraints "tags="
+juju bootstrap --show-log --upload-tools
 juju deploy --to 0 --repository=/srv/charmstore/ local:precise/juju-gui
 juju expose juju-gui
 
