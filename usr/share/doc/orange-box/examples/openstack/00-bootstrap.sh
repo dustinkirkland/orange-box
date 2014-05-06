d2a27d36c855281650e06aeb9b64e47f1841e29d
Dustin Kirkland <dustin.kirkland@gmail.com>
2014-05-06 01:03:05 -0500

    * debian/postinst, debian/preinst, usr/share/doc/orange-   box/examples/openstack/00-bootstrap.sh:   - resurrect virtual machine creation   - use manual in /etc/network/interfaces to work around network manager     crashing

--- usr/share/doc/orange-box/examples/openstack/00-bootstrap.sh
+++ usr/share/doc/orange-box/examples/openstack/00-bootstrap.sh
@@ -5,9 +5,8 @@
 set -x
 
 juju status && echo "Existing deployment found, exiting." && exit 0
-#juju bootstrap --constraints "tags=virtual" --show-log --upload-tools
-#juju set-constraints "tags="
-juju bootstrap --show-log --upload-tools
+juju bootstrap --constraints "tags=virtual" --show-log --upload-tools
+juju set-constraints "tags="
 juju deploy --to 0 --repository=/srv/charmstore/ local:precise/juju-gui
 juju expose juju-gui
 
